a1301d547c103081740b474afbad6d31e605e409
anneloviseramstad <160476303+anneloviseramstad@users.noreply.github.com>
2024-12-11 12:40:03 +0100

    html, styles and js is added to my portfolio.

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function viewProjectOne() {
+  window.open(
+    "https://anneloviseramstad.github.io/javascript1-anneramstad/",
+    "_blank"
+  );
+}
+
+function viewCodeOne() {
+  window.open(
+    "https://github.com/anneloviseramstad/javascript1-anneramstad",
+    "_blank"
+  );
+}
+
+function viewProjectTwo() {
+  window.open(
+    "https://anneloviseramstad.github.io/semester-project-anneloviseramstad/",
+    "_blank"
+  );
+}
+
+function viewCodeTwo() {
+  window.open(
+    "https://github.com/anneloviseramstad/semester-project-anneloviseramstad",
+    "_blank"
+  );
+}
+
+function viewProjectThree() {
+  window.open("https://culinarycreationsblog.netlify.app/", "_blank");
+}
+
+function viewCodeThree() {
+  window.open(
+    "https://github.com/anneloviseramstad/project-exam-1-anneramstad",
+    "_blank"
+  );
+}
